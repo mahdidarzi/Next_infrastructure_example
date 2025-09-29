17261a42644cd5f1b23f6a9b7504197a3928dc5b
mhubsources <mahdidarzi1024@gmail.com>
2025-09-29 14:27:45 +0330

    chore: extra comments has been removed

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,12 @@
-// import { Preview } from "@storybook/nextjs";
-// import '../app/globals.scss';
-
-// const preview: Preview = {
-  
-//   tags: ['autodocs'],
-// };
-
-// export default preview;
-
 import { Preview } from "@storybook/nextjs";
 import '../app/globals.scss';
-import React from 'react';
 
 const withThemeProvider = (Story: any, context: any) => {
-  // Use Storybook toolbar theme, default to 'light'
+
   const theme = context.globals.theme || 'dark';
-  
+
   return (
-    <div data-theme={theme} style={{ backgroundColor:theme==='dark'?'black':'white'}}>
+    <div data-theme={theme} style={{ backgroundColor: theme === 'dark' ? 'black' : 'white' }}>
       <Story {...context} />
     </div>
   );
